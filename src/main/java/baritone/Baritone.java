00d8083bc9c21bdc7bd75acae02523c6d0d7ae1a
cmg-divined <72712219+cmg-divined@users.noreply.github.com>
2025-04-19 12:16:57 +0300

    Update Baritone.java

--- src/main/java/baritone/Baritone.java
+++ src/main/java/baritone/Baritone.java
@@ -3,7 +3,7 @@
  *
  * Baritone is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundations, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Baritone is distributed in the hope that it will be useful,
